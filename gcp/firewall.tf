05fff646946be0863580be013d491b77b2778701
Raghu <raghu.esh@gmail.com>
2024-11-28 14:11:06 +0530

    Create firewall.tf

---
+++ gcp/firewall.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  project     = "raghu-test-1"
+  region      = "us-central1"
+}
+
+resource "google_compute_firewall" "good_firewall"  {
+  name    = "good-firewall" 
+  network = var.network
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  source_ranges = ["1.1.1.1/32"]
+}
+
+# orca-iac ignore-block
+resource "google_compute_firewall" "bad_firewall"  {
+  name    = "bad-firewall" 
+  network = var.network
+  allow {
+    protocol = "tcp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
